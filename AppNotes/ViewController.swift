9ba9c73f32f46c748ded2ec2a32785347527369b
alexkolinko <as.kolinko@gmail.com>
2019-05-19 19:38:37 +0300

    Initial Commit

---
+++ AppNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppNotes
+//
+//  Created by Alexandr on 5/19/19.
+//  Copyright © 2019 Alex Kolinko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
